0a783c537a74ba0b4077091c08ae6d420bcd08f5
Udit Khemka <58941463+uditkhemka@users.noreply.github.com>
2020-07-31 02:06:58 +0530

    Add files via upload

---
+++ weatherofloc.js
@@ -0,0 +1,83 @@
+var lat=25,longt=30,place;
+
+window.addEventListener('load', ()=> {
+  var x = document.getElementById("demo");
+  
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  
+} else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+}
+
+
+
+function showPosition(position) {
+  longt=position.coords.longitude;
+  lat=position.coords.latitude;
+  x.innerHTML = longt;
+//console.log(longt);
+//longt=document.getElementById("demo");
+  $(document).ready(function(){     //this code running before page loading so put inside showPositition
+    var lat1=lat;
+    var longt1=longt;
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://us1.locationiq.com/v1/reverse.php?key=d205f7d032efd1&lat="+lat1+"&lon="+longt1+"&format=json",    
+      "method": "GET"
+    }
+
+  //here we find the city of current location
+    $.ajax(settings).done(function (data) {
+      console.log(data);
+      
+    });
+
+    //now we use the current city
+    
+  }); 
+
+}
+
+function showError(error) {
+switch(error.code) {
+  case error.PERMISSION_DENIED:
+    x.innerHTML = "Please allow Location Access!!!"
+    var cnt=1;
+    setInterval(() => {
+          cnt++;
+          if(cnt > 2)     window.close();
+     }, 1000);
+      
+    break;
+  case error.POSITION_UNAVAILABLE:
+    x.innerHTML = "Location information is unavailable."
+    var cnt=1;
+    setInterval(() => {
+          cnt++;
+        if(cnt > 2)     location.replace('search.html')
+     }, 1000);
+    break;
+  case error.TIMEOUT:
+    x.innerHTML = "The request to get user location timed out."
+    var cnt=1;
+    setInterval(() => {
+          cnt++;
+        if(cnt > 2)     location.replace('search.html')
+     }, 1000);
+    break;
+  case error.UNKNOWN_ERROR:
+    x.innerHTML = "An unknown error occurred."
+    var cnt=1;
+    setInterval(() => {
+          cnt++;
+        if(cnt > 2)     location.replace('search.html')
+     }, 1000);
+    break;
+}
+}
+
+  
+});
