c0d7d98242cf2cbdcb4db2b49c494f7c8e7d1c4c
Udit Khemka <58941463+uditkhemka@users.noreply.github.com>
2020-08-13 20:47:32 +0530

    Update saction.js

--- saction.js
+++ saction.js
@@ -1,4 +1,12 @@
+var place;
+
+
 function function1(){
+    place=document.getElementById("userinput");
+    console.log(place);
+    localStorage.setItem("city1",place);
+    //localStorage.setItem("value",0);
+   // window.location.href="weatherofcity.html";
     window.open('weatherofcity.html');
 }
 
