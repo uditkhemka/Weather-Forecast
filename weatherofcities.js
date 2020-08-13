d928efb419fbcca1dc5eb844f34fbb9ee2e62405
Udit Khemka <58941463+uditkhemka@users.noreply.github.com>
2020-08-13 20:53:37 +0530

    Add files via upload

---
+++ weatherofcities.js
@@ -0,0 +1,25 @@
+var city,retrieve=0;
+retrieve=localStorage.getItem("value");
+if(retrieve==1){
+    city=localStorage.getItem("city");
+    console.log(city);
+}
+else{
+    city=localStorage.getItem("city1");
+    console.log(city);
+}
+
+//localStorage.setItem("city",resetValue);
+
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=881fa4d8770aa81ebcc14abc786aa0aa",function(data){
+    console.log(data);
+    var icon= "https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+    console.log(icon);
+    var weather= data.weather[0].main;
+    var temp=data.main.temp;
+    $('.icon').attr('src', icon);
+    
+    $('.temp').append(temp);
+    $('.weather-info').append(weather);
+});
