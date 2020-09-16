bf133ca9904c6d3c6b3b7ebd094f45b264e40d88
Udit Khemka <58941463+uditkhemka@users.noreply.github.com>
2020-09-16 20:29:36 +0530

    Update weatherofcities.js

--- weatherofcities.js
+++ weatherofcities.js
@@ -1,11 +1,13 @@
-var city,retrieve=0;
+var city,retrieve=-1;
 retrieve=localStorage.getItem("value");
+//retrieve=0;
+console.log(retrieve);
 if(retrieve==1){
     city=localStorage.getItem("city");
     console.log(city);
 }
 else{
-    city=localStorage.getItem("city1");
+    city=localStorage.getItem("city");
     console.log(city);
 }
 
@@ -15,7 +17,7 @@ else{
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=881fa4d8770aa81ebcc14abc786aa0aa",function(data){
     console.log(data);
     var icon= "https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
-    console.log(icon);
+    //console.log(icon);
     var weather= data.weather[0].main;
     var temp=data.main.temp;
     $('.icon').attr('src', icon);
